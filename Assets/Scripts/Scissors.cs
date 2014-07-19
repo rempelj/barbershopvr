db9771948c9399d8093409d514f46f1e2af01259
Brody Holden <justinrempel@gmail.com>
2014-07-19 14:54:09 -0700

    Snip every 2 seconds

--- Assets/Scripts/Scissors.cs
+++ Assets/Scripts/Scissors.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class Scissors : MonoBehaviour {
 
+	public Animation snipAnimation;
+	
+	public float snipRate;
+	
+	private float lastSnipTime;
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Time.time > lastSnipTime + snipRate) {
+			lastSnipTime = Time.time;
+			Snip();
+		}
+	}
 	
+	public void Snip()
+	{
+		snipAnimation.Play();
+		// play sfx
 	}
 }
