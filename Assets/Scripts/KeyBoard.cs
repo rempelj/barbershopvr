0bfc190e881bf57b773c367866f8f4689c57537d
Brody Holden <brody.holden.r@gmail.com>
2014-07-19 22:35:30 -0700

    Press spacebar to reset.
    Signed-off-by: Brody Holden <brody.holden.r@gmail.com>

---
+++ Assets/Scripts/KeyBoard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyBoard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			Debug.Log("Space key was pressed.");
+			Application.LoadLevel("GameplayScene");
+		}
+	}
+}
