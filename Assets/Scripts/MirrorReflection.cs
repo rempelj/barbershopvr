a659eabe85b1058949f805a73aa9e0e0e4d9967b
Brody Holden <brody.holden.r@gmail.com>
2014-07-19 18:50:17 -0700

    Rest parameter to original value.
    Signed-off-by: Brody Holden <brody.holden.r@gmail.com>

--- Assets/Scripts/MirrorReflection.cs
+++ Assets/Scripts/MirrorReflection.cs
@@ -28,7 +28,7 @@ using System.Collections;
 public class MirrorReflection : MonoBehaviour
 {
     public bool m_DisablePixelLights = true;
-    public int m_TextureSize = 512; //256;
+    public int m_TextureSize = 256;
     public float m_ClipPlaneOffset = 0.07f;
  
     public LayerMask m_ReflectLayers = -1;
